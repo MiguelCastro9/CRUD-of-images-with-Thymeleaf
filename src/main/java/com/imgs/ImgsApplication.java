69a1255a7d1b3e70f6dcbe33e8ec9da778ddd1b0
miguel castro <miguel.castro9@hotmail.com>
2022-09-12 20:59:51 -0300

    config inicial

---
+++ src/main/java/com/imgs/ImgsApplication.java
@@ -0,0 +1,13 @@
+package com.imgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imgs/ImgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
